43686e5514f9188bcf6abe874fcbd3b6e19db82d
kavin <kavink57@gmail.com>
2020-12-07 15:14:03 +0530

    updated to 2021

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-var countDate = new Date('Jan 1, 2020 00:00:00').getTime();
-
+var countDate = new Date('Jan 1, 2021 00:00:00').getTime();
+var check = 0;
 function newYear() {
 
     var now = new Date().getTime();
     gap = countDate - now;
-
     let second = 1000;
     let minute = second * 60;
     let hour = minute * 60;
@@ -14,14 +13,19 @@ function newYear() {
     var h = Math.floor((gap % (day)) / (hour));
     var m = Math.floor((gap % (hour)) / (minute));
     var s = Math.floor((gap % (minute)) / second);
-
+    console.log(d,m,h,s)
     document.getElementById('day').innerHTML = d;
     document.getElementById('hour').innerHTML = h;
     document.getElementById('minute').innerHTML = m;
     document.getElementById('second').innerHTML = s;
+
+    if (d,h,m,s == check){
+        window.location.href = '/newyea_2020.html';
+    }else
+        console.log('Error');
     
 }
 setInterval(() => {
     newYear();
-}, 1000);
+}, 1500);
 
